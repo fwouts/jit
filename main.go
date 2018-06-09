dcefc16d797e837fdd30d15ccba32b5f23493f75
Francois Wouts <f@zenc.io>
2018-06-10 09:58:26 +1000

    Add a marker in front of Jira issues that have a local branch

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"github.com/andygrunwald/go-jira"
+	"github.com/deckarep/golang-set"
 	"github.com/fatih/color"
+	"github.com/libgit2/git2go"
 	"gopkg.in/AlecAivazis/survey.v1"
 	"log"
+	"os"
+	"regexp"
 	"strings"
 )
 
 func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	repoPath := flag.String("repo", cwd, "Path to the git repository")
 	jiraHost := flag.String("jira-host", "", "URL of the Jira Cloud host")
 	jiraUsername := flag.String("jira-user", "", "Jira username")
 	jiraToken := flag.String("jira-token", "", "Jira API token")
@@ -24,6 +33,34 @@ func main() {
 	}
 	if *jiraToken == "" {
 		log.Fatal("Please set a Jira API token with --jira-token.")
+	}
+
+	repo, err := git.OpenRepository(*repoPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	branchPrefixes := mapset.NewSet()
+	i, err := repo.NewBranchIterator(git.BranchLocal)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jiraIssueFormat, err := regexp.Compile("^([A-Z]+-[0-9]+)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = i.ForEach(func(b *git.Branch, t git.BranchType) error {
+		name, err := b.Name()
+		if err != nil {
+			return err
+		}
+		res := jiraIssueFormat.FindStringSubmatch(name)
+		if len(res) > 0 {
+			branchPrefixes.Add(res[1])
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	tp := jira.BasicAuthTransport{
@@ -54,7 +91,13 @@ func main() {
 		default:
 			colouredPriority = color.GreenString(issue.Fields.Priority.Name)
 		}
-		choices[i] = fmt.Sprintf("%s %v (%s): %+v", issue.Key, colouredPriority, issue.Fields.Type.Name, issue.Fields.Summary)
+		var summary string
+		if branchPrefixes.Contains(issue.Key) {
+			summary = fmt.Sprintf("*%s %v (%s): %+v", issue.Key, colouredPriority, issue.Fields.Type.Name, issue.Fields.Summary)
+		} else {
+			summary = fmt.Sprintf("%s %v (%s): %+v", issue.Key, colouredPriority, issue.Fields.Type.Name, issue.Fields.Summary)
+		}
+		choices[i] = summary
 	}
 	questions := []*survey.Question{
 		{
